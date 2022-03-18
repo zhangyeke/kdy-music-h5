15fe27ffe970e6a88079bebab583a08fc07800a3
jffdss <jiangdota8@outlook.com>
2022-03-18 18:11:57 +0800

    练手

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-16 19:45:06
- * @LastEditTime: 2022-03-17 15:39:48
+ * @LastEditTime: 2022-03-18 10:16:00
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \zyk-music-h5\src\components\HelloWorld.vue
@@ -29,7 +29,7 @@
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p class="color_red">
+  <p class="primary_color">
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.1
   </p>
