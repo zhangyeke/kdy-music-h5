056908c22c1adc91ef90586ae401a046afb9e6c9
jffdss <jiangdota8@outlook.com>
2022-04-14 18:00:03 +0800

    更新弹窗

--- src/components/kdy-bottom-play/kdy-bottom-play.vue
+++ src/components/kdy-bottom-play/kdy-bottom-play.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-04-07 20:35:32
- * @LastEditTime: 2022-04-13 17:59:18
+ * @LastEditTime: 2022-04-14 17:48:02
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \zyk-music-h5\src\components\kdy-bottom-play\kdy-bottom-play.vue
@@ -34,15 +34,33 @@
     </div>
     <var-style-provider :style-vars="{ '--popup-content-background-color': 'transparent' }">
       <var-popup v-model:show="showSongList" position="bottom">
-        <div class="song h-400px bg-white p-10px">
-          <div class="song_title">
-            <span class="text-[#333] text-16px font-700 tracking-wide">当前播放</span>
-            <span class="text-[#999] text-12px">(12)</span>
-          </div>
-          <div class="tool_bar flex justify-between">
-            <div>
-              
+        <div class="song h-400px bg-white  flex flex-col">
+          <div class="popup_head py-10px px-15px">
+            <div class="song_title">
+              <span class="text-[#333] text-16px font-700 tracking-wide">当前播放</span>
+              <span class="text-[#999] text-12px">(12)</span>
             </div>
+            <div class="tool_bar flex justify-between mt-10px">
+              <div class="flex items-center">
+                <var-icon namespace="kdy-icon" :name="cycle_types[cycle_cur].icon" />
+                <span class="text-14px text-[#333] ml-5px">{{ cycle_types[cycle_cur].name }}</span>
+              </div>
+              <div class="flex items-center">
+                <div v-for="(item, index) in tool_bars" :key="index" class="mr-15px">
+                  <var-icon namespace="kdy-icon" :name="item.icon" :size="tool.px2vw(20)" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="popup_body flex-1 overflow-y-scroll mt-10px  px-15px">
+            <div class="song_list">
+              <div v-for="(item, index) in 15" :key="index">第{{item}}项</div>
+            </div>
+          </div>
+          <div class="bg-warning-dark h-40px text-14px flex items-center px-20px">
+            <span class="bg-[#000] rounded-10px py-3px px-10px  text-warning">vip</span>
+            <span class="flex-1 ml-5px text-[#333]">含1首VIP专享歌曲</span>
+            <span class="text-danger font-700">VIP首月3.8元</span>
           </div>
         </div>
       </var-popup>
@@ -66,25 +84,38 @@ let audio_length = ref(0)
 // 播放器组件
 let kdy_audio = ref<typeof kdyAudio | null>(null)
 // 显示歌单
-let showSongList = ref(true)
+let showSongList = ref(false)
 // 循环类型
 let cycle_types = ref([
   {
     name: "列表循环",
     icon: "liebiaoxunhuan",
-    value:1,
+    value: 1,
   }, {
     name: "随机播放",
     icon: "suijibofang1",
-    value:2
+    value: 2
   }, {
     name: "单曲循环",
     icon: "danquxunhuan",
-    value:1,
+    value: 1,
   }
 ])
 // 单曲循环类型
 let cycle_cur = 0
+
+// 下载 收藏歌单 清空播放列表
+let tool_bars = [
+  {
+    icon: "xiazai"
+  },
+  {
+    icon: "tianjiashoucang"
+  },
+  {
+    icon: "lajixiang"
+  }
+]
 
 onMounted(() => {
 
