2d016a8b32837a7f04f07b6d0e1a203c8ec42c3b
zyk <997610780@qq.com>
2023-02-09 16:30:29 +0800

    varlet组件库升级

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-03-16 19:45:06
- * @LastEditTime: 2022-05-17 17:28:48
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-02-09 16:28:56
+ * @LastEditors: zyk 997610780@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \zyk-music-h5\src\main.ts
  */
@@ -12,8 +12,9 @@ import pinia from "@/store/index";
 import 'virtual:windi.css';
 // import "@/assets/style/global.scss"
 import "@/assets/style/font.css";
-import '@varlet/ui/es/style.js';
+import '@varlet/ui/es/style';
+import Varlet from '@varlet/ui';
 const app = createApp(App)
 
-app.use(pinia).use(router).mount('#app')
+app.use(pinia).use(Varlet).use(router).mount('#app')
 
