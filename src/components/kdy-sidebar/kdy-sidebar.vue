16b65d72cd50a7885bad0941568f15dd9a9480f1
zyk <997610780@qq.com>
2023-05-11 18:14:50 +0800

    侧边栏弹窗调整

--- src/components/kdy-sidebar/kdy-sidebar.vue
+++ src/components/kdy-sidebar/kdy-sidebar.vue
@@ -1,49 +1,67 @@
 <!--
  * @Author: kdy
- * @Date: 2022-03-24 17:47:16
+ * @Date: 322-03-24 17:47:16
  * @LastEditTime: 2023-03-21 21:40:43
  * @LastEditors: 可达鸭 997610780@qq.com
  * @Description:侧边栏
  * @FilePath: \zyk-music-h5\template.vue
 -->
 <template>
-  <div class="sidebar" >
-    <var-style-provider :style-vars="styleVars">
-      <var-popup position="left" :show="show" @close="close" @click-overlay="clickOverlay" @open="open">
-        <div class="sidebar_window px-20px py-30px w-300px">
-          <div class="sidebar_window_head flex items-center justify-between">
-            <div class="flex items-center" v-if="userStore.token">
-              <img class="w-30px rounded-1/2 fit_cover"
-                :src="userStore.userInfo.avatarUrl ? userStore.userInfo.avatarUrl : tool.getAssetsImages('image/default_avatar.jpg')">
-              <div class="flex items-center flex-1 ml-10px text-14px text-[#333]">
-                <span>{{ userStore.userInfo.nickname ? userStore.userInfo.nickname : userStore.userInfo.userName}}</span>
-                <var-icon name="chevron-right" color="#333" />
-              </div>
-            </div>
-            <div class="flex items-center" v-else @click="toLogin">
-              <img class="w-30px rounded-1/2 fit_cover" :src="tool.getAssetsImages('image/default_avatar.jpg')">
-              <div class="flex items-center flex-1 ml-10px text-14px text-[#333]">
-                <span>立即登录</span>
-                <var-icon name="chevron-right" color="#333" />
-              </div>
-            </div>
-            <!-- 扫码 -->
-            <div>
-              <var-icon namespace="kdy-icon" name="saoma1" color="#333" size="20" />
-            </div>
-          </div>
-          <div class="sidebar_window_body overflow-y-scroll h-full" v-if="userStore.token">
-            <div class="w-8/10 mt-15px mx-auto">
-              <var-button class="w-full" @click="logout"><span class="text-primary">退出登录</span></var-button>
-            </div>
-          </div>
+  <var-popup position="left" :show="show" @close="close" @click-overlay="clickOverlay" @open="open">
+    <div class="sidebar px-20px py-30px w-300px h-100vh">
 
+      <div class="sidebar_hd" @click="clickSidebarHd">
+        <template v-if="userStore.token">
+          <div class="avatar">
+            <var-image width="100%" height="100%" object="cover" radius="50%"
+              :src="userStore.userInfo.avatarUrl"></var-image>
+          </div>
+          <div class="user_name">{{ userStore.userInfo.nickname || userStore.userInfo.userName }}</div>
+
+          <div class="">
+            <var-chip plain type="primary" :round="false" size="mini" class="mr-5px">
+              <span>{{ tool.timeFormat(userStore.userInfo.birthday, 'YYYY').substring(2, 3) }}0后</span>
+            </var-chip>
+            <var-chip plain type="primary" :round="false" size="mini">{{
+              constellations[tool.timeFormat(userStore.userInfo.birthday, 'YYYY-M').split('-')[1] - 1].name }}</var-chip>
+          </div>
+        </template>
+        <template v-else>
+          <div class="avatar">
+            <var-image width="100%" height="100%" object="cover" radius="50%"
+              :src="tool.getAssetsImages('image/default_avatar.jpg')"></var-image>
+          </div>
+          <div class="user_name">未登录</div>
+        </template>
+      </div>
+      <div class="sidebar_by">
+        <div class="cell" v-for="(item, index) in cell_list" :key="index" v-ripple @click="cellClick(item)" >
+          <span>{{ item.name }}</span>
+          <var-icon name="chevron-right" />
         </div>
-      </var-popup>
-    </var-style-provider>
-  </div>
+        <div class="w-8/10 mt-15px mx-auto" v-show="userStore.token">
+          <var-button class="w-full" @click="logout"><span class="text-[var(--color-danger)]">退出登录</span></var-button>
+        </div>
+      </div>
+      <div class="sidebar_ft text-12px absolute bottom-50px" >
+        <div class="flex items-center  text-[#ddd] leading-20px">
+          <span>喜欢这个项目吗？帮助我拯救流浪猫和狗，就在GitHub和Gitee上点一下Star吧！</span>
+          <!-- <var-icon name="star" color="#eac54f" :size="tool.px2vw(16)"/> -->
+        </div>
+        <div class="flex items-center justify-center">
+          <var-icon name="github" :size="tool.px2vw(34)" class="mr-10px" @click="jumpStar('https://github.com/zhangyeke/kdy-music-h5.git')"/>
+          <var-icon name="mayun" namespace="kdy-icon" :size="tool.px2vw(28)" @click="jumpStar('https://gitee.com/zhang-yeke/zyk-music-h5.git')"/>
+          <!-- <span></span> -->
+        </div>
+        <div class="flex items-center">
+          <!-- <span></span> -->
+        </div>
+      </div>
+    </div>
+  </var-popup>
 </template>
 <script setup lang="ts">
+import constellations from "@/enum-file/constellation.json";
 import { Dialog } from '@varlet/ui';
 import useUserStore from "@/store/user";
 let prop = defineProps({
@@ -53,17 +71,66 @@ let prop = defineProps({
   }
 })
 let emit = defineEmits(['update:show', 'close', 'click-overlay', 'open'])
-const styleVars = {
-  '--popup-content-background-color': '#f8f8f8'
-}
+
 let router = useRouter()
 let userStore = useUserStore()
 let tool = useTool()
+let cell_list = reactive([
+  {
+    name: "每日推荐",
+    url: "/everyDayRmd",
+  },
+  {
+    name: "我的歌单",
+    url: {
+      name:"userPlaylist",
+      params:{
+        nickname:userStore.userInfo.nickname || userStore.userInfo.userName,
+        id: userStore.userId,
+        type:0
+      }
+    }
+  },
+  {
+    name: "我的好友",
+    url: "/goodFriend",
+  },
+  {
+    name: "我的收藏",
+    url: "/collect",
+  },
+  {
+    name: "分享" + import.meta.env.VITE_APP_TITLE,
+  }
+])
 
-const toLogin = ()=>{
-  close()
-  router.push({ path: '/login' })
+const jumpStar = (url:string)=>{
+  window.open(url, '_blank','height=600,width=800,top=100,left=100');
 }
+
+const cellClick = <P>(item: { name: string, url?: string | P }) => {
+  close()
+  if (item.url) {
+    router.push(item.url)
+  } else {
+    tool.share({
+      desc: "发现和分享新的音乐体验",
+      title:import.meta.env.VITE_APP_TITLE,
+      link:location.origin,
+    })
+  }
+
+}
+
+const clickSidebarHd = () => {
+  close()
+  if (userStore.token) {
+    router.push({ name: "userDetail", params: { id: userStore.userId } })
+  } else {
+    router.push({ path: '/login' })
+  }
+}
+
 
 // 关闭弹层
 const close = () => {
@@ -91,9 +158,10 @@ const logout = () => {
     cancelButtonTextColor: "#666",
     onConfirm: () => {
       userStore.logout().then(_ => {
+        close()
         tool.toast({ type: 'success', content: "退出成功!" })
         setTimeout(() => {
-          router.replace({ path: "/login" })
+          router.push("/login" )
         }, 1500);
       })
     },
@@ -106,5 +174,33 @@ const logout = () => {
 </script>
 
 <style scoped lang="scss">
+.sidebar {
+  background: url(@/assets/image/side_bg.jpg) no-repeat center/cover;
+  @apply bg-white text-white;
 
+  &_hd {
+    @apply flex items-center flex-col;
+
+    .avatar {
+      width: 80px;
+      height: 80px;
+    }
+
+    .user_name {
+      font-size: 14px;
+      margin-top: 5px;
+    }
+  }
+
+  &_by {
+    margin-top: 10px;
+  }
+
+  .cell {
+    @apply flex justify-between items-center border-b;
+    padding: 5px 0;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+}
 </style>
